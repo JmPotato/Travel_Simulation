4f01ab6d530b6e2aa23fa131954973871bec0eb1
rinchannow <691182679@qq.com>
2019-03-17 16:32:31 +0800

    update

--- Travel_Simulation/Strategy.cpp
+++ Travel_Simulation/Strategy.cpp
@@ -1,6 +1,24 @@
 #include "Strategy.h"
 
-Strategy::Strategy()
+Strategy::Strategy(int t, string d1, string d2, Time T)
 {
+    type = t;
+    depart = d1;
+    dest = d2;
+    expectedTime = T;
+}
 
+void Strategy::startStrategy()
+{
+    switch (type) {
+        case 1:
+            cheapestStrategy();
+            break;
+        case 2:
+            fastestStrategy();
+            break;
+        case 3:
+            timeLimitStrategy();
+            break;
+    }
 }
--- Travel_Simulation/Strategy.h
+++ Travel_Simulation/Strategy.h
@@ -1,11 +1,31 @@
 #ifndef STRATEGY_H
 #define STRATEGY_H
 
+#include <string>
+#include "Result.h"
+#include "Time.h"
+
+using std::string;
 
 class Strategy
 {
 public:
-    Strategy();
+    Strategy(int t, string d1, string d2, Time T);
+
+    void startStrategy();
+private:
+    int type;
+    string depart;
+    string dest;
+    Time expectedTime;
+
+    Result result;
+
+    // 三种策略
+    void cheapestStrategy();
+    void fastestStrategy();
+    void timeLimitStrategy();
+
 };
 
 #endif // STRATEGY_H
