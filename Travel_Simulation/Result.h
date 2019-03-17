c4ab7d8cb447a5c0071e8fbc51ca247a01d13de2
rinchannow <691182679@qq.com>
2019-03-17 16:02:52 +0800

    add header

---
+++ Travel_Simulation/Result.h
@@ -0,0 +1,4 @@
+#ifndef RESULT_H
+#define RESULT_H
+
+#endif // RESULT_H
---
+++ Travel_Simulation/Strategy.h
@@ -0,0 +1,11 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+
+
+class Strategy
+{
+public:
+    Strategy();
+};
+
+#endif // STRATEGY_H
