5c3ec844381796c5d48a738f83264cdc3007210f
JmPotato <ghzpotato@gmail.com>
2019-03-27 14:26:35 +0800

    可以通过 GUI 选择策略进行模拟了

--- Travel_Simulation/mainwindow.cpp
+++ Travel_Simulation/mainwindow.cpp
@@ -8,8 +8,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->endTime->setEnabled(false);
     ui->startTime->setDate(QDate::currentDate());
+    ui->endTime->setEnabled(false);
+    ui->budgetEdit->setEnabled(false);
+    QStringList strategyList = {"最少费用", "最少用时", "最少费用+时间"};
+    ui->strategyBox->addItems(strategyList);
     QStringList cityList = {"上海", "北京","南京" ,"广州" ,"成都" ,"杭州" ,"武汉" ,"深圳" ,"西安" ,"郑州" ,"重庆" ,"青岛"};
     ui->departureBox->addItems(cityList);
     ui->destinationBox->addItems(cityList);
@@ -22,15 +25,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_planButton_clicked() {
     MyTime startTime(0, ui->startTime->time().hour(), ui->startTime->time().minute());
-    Tourist t(ui->departureBox->currentText().toStdString(), ui->destinationBox->currentText().toStdString(), startTime, 2);
+    Tourist t(ui->departureBox->currentText().toStdString(), ui->destinationBox->currentText().toStdString(), startTime, ui->strategyBox->currentIndex() + 1);
     t.getStrategy();
     ui->logBrowser->setText(t.getLog());
     MyTime endTime = startTime + t.getPlanResult()->destTime - t.getPlanResult()->expectedDepartTime;
-//    QDateTime desTime = ui->startTime->dateTime();
-//    MyTime usedTime = t.getPlanResult()->destTime - t.getPlanResult()->expectedDepartTime;
-//    desTime.setTime(desTime.time().addSecs(usedTime.hour * 3600 + usedTime.minute * 60));
-//    desTime.setDate(desTime.date().addDays(usedTime.day));
-//    ui->endTime->setDateTime(desTime);
     ui->endTime->setDate(ui->startTime->date().addDays(endTime.day));
     ui->endTime->setTime(QTime::fromString("00:00", "hh:mm"));
     ui->endTime->setTime(ui->endTime->time().addSecs(endTime.hour * 3600 + endTime.minute * 60));
