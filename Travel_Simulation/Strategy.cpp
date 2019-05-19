650d831e92fe79cc742eb3493b76f82e8b8da81c
rinchannow <691182679@qq.com>
2019-05-19 20:23:40 +0800

    timelimit strategy  bugs fixed

--- Travel_Simulation/Strategy.cpp
+++ Travel_Simulation/Strategy.cpp
@@ -815,7 +815,10 @@ Result Strategy::timeLimitStrategy(QString &log)
                                     }
                                     currentTime = endTime;
                                     currentTime.day = 0;
-                                    oneResult.route.push_back(allMethodForOneRoute[index][p]);
+                                    Path tempPath = allMethodForOneRoute[index][p];
+                                    tempPath.endTime.day = endTime.day;
+                                    tempPath.startTime = tempPath.endTime - tempPath.timeCost;
+                                    oneResult.route.push_back(tempPath);
                                     oneResult.moenyCost += allMethodForOneRoute[index][p].moneyCost;
                                 }
                                 oneResult.timeCost = endTime - expectedDepartTime;
@@ -881,11 +884,7 @@ Result Strategy::timeLimitStrategy(QString &log)
                 log.append(QString("路线: %1--->%2\n").arg(QString::fromStdString(it->start)).arg((QString::fromStdString(it->end))));
                 log.append(QString("交通工具: %1    ").arg(it->tool));
                 log.append(QString("车次/航班号: %1\n").arg(it->number));
-                log.append(QString("花费金额: %1\n").arg(it->moneyCost));
-                if (it->endTime.day == 0)
-                    log.append(QString("出发时间: %1时%2分 到达时间: %3时%4分\n").arg(it->startTime.hour).arg(it->startTime.minute).arg(it->endTime.hour).arg(it->endTime.minute));
-                else
-                    log.append(QString("到达时间: 第%3天%4时%5分\n").arg(it->endTime.day + 1).arg(it->endTime.hour).arg(it->endTime.minute));
+                log.append(QString("花费金额: %1\n").arg(it->moneyCost));                    log.append(QString("出发时间: %1时%2分 到达时间: %3时%4分\n").arg(it->startTime.hour).arg(it->startTime.minute).arg(it->endTime.hour).arg(it->endTime.minute));
                 log.append(QString("用时: %1天%2时%3分\n\n").arg(it->timeCost.day).arg(it->timeCost.hour).arg(it->timeCost.minute));
             }
             log.append(QString("总用时: %1天%2时%3分\n").arg(result.timeCost.day).arg(result.timeCost.hour).arg(result.timeCost.minute));
